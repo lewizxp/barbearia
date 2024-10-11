71261dcf374582e9351e192423d60dec4cb7be25
Programador Full Stack 2024.1 <16133232024.1@senacrjedu.local>
2024-10-10 21:46:51 -0300

    terminar

---
+++ js/main.js
@@ -0,0 +1,15 @@
+navBarComponemt()
+
+function navBarComponemt(){
+    const element = document.getElementById(`navbar-component`)
+    const navbar =`<nav class="navbar">
+    <ul class="nav-links">
+        <li><a href="index.html">Início</a></li>
+        <li><a href="#services">Serviços</a></li>
+        <li><a href="#about">Sobre Nós</a></li>
+        <li><a href="#contact">Contato</a></li>
+        <li><a href="login.html">Login</a></li>
+    </ul>
+</nav>`
+element.innerHTML= navbar
+}
