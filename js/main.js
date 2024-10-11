e7cdab2348d104a0ea6d75e2060c5308804880d8
Programador Full Stack 2024.1 <16133232024.1@senacrjedu.local>
2024-10-10 21:47:33 -0300

    feito

--- js/main.js
+++ js/main.js
@@ -13,3 +13,27 @@ function navBarComponemt(){
 </nav>`
 element.innerHTML= navbar
 }
+
+
+cards()
+function cards(){
+    const element= document.getElementById(`card`)
+    const card = `<div class="card">
+    <div class="card-img">
+        <img src="./imagens/barber-logo.jpg" alt="">
+    </div>
+
+    <div class="card-content">
+        <h2>Barba</h2>
+        <p>
+            É uma forma de expressar a personalidade, realçar os traços do rosto e acompanhar as últimas
+            tendências da moda masculina.
+        </p>
+    </div>
+</div>`
+
+element.innerHTML = card
+for (i = 0 ; i card.length; i++){
+    card[i] = card [i];
+}
+}
