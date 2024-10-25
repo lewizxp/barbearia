21a496a831d142fe04e0378d17736ec13edb873b
lewizxp <luizpedrosantos297@gmail.com>
2024-10-25 19:09:15 -0300

    quse

--- js/main.js
+++ js/main.js
@@ -1,62 +1,52 @@
-navBarComponemt()
+//navBarComponent()
+navBarComponentTemplate()
 
-function navBarComponemt(){
-    const element = document.getElementById(`navbar-component`)
-    const navbar =  
-    <nav class="navbar navbar-expand-lg fixed-top ">
+function navBarComponent() {
+    const element = document.getElementById('navbar-component')
+    const navbar = `
+    <nav class="navbar navbar-expand-lg fixed-top">
         <div class="container">
-            /*<a class="navbar-brand" href="#">Navbar</a>*/
-           
-            
+            <!-- <a class="navbar-brand" href="#">Navbar</a> -->
+            <img class="navbar-toggler border-0" src="./imagem/barbershop-logo.jpg" height="40" alt="">
             <button class="navbar-toggler bg-yellow" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
                 aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
             <div class="collapse navbar-collapse" id="navbarNav">
-
-                <ul class="navbar-nav me-auto ">
+                <ul class="navbar-nav me-auto">
                     <li class="nav-item">
-                        <a class="nav-link " aria-current="page" href="index.html">Home</a>
+                        <a class="nav-link" href="index.html">Inicio</a>
                     </li>
-
                     <li class="nav-item">
-                        <a class="nav-link " aria-current="page" href="#services">Serviços</a>
+                        <a class="nav-link" href="index.html#services">Serviços</a>
                     </li>
-
                     <li class="nav-item">
-                        <a class="nav-link " aria-current="page" href="#about">Sobre Nós</a>
+                        <a class="nav-link" href="index.html#about">Sobre Nós</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="index.html#contact">Contato</a>
                     </li>
                 </ul>
-
-                <ul class="navbar-nav ">
-                     <li  class="nav-item">
-                            <a class="nav-link "  href= "login.html">Login </a>
-                     </li>
-                    
-                    
+                <ul class="navbar-nav">
+                    <li class="nav-item">
+                        <a class="nav-link" href="login.html">Entrar</a>
+                    </li>
                 </ul>
-
             </div>
         </div>
     </nav>
-
-element.innerHTML= navbar
-}
-function navBarComponent (){
- const element = document.getElementById ("nav-component")
- const navbar =`
- `
-}
-function navBarComponentTemplate(){
-    const element = document.getElementById( "nav-component")
-    fetch ("../components/nav-component.html")
-    .then ( (res)=> res.text())
-    .then((nav) => {
-        element.innerHTML - nav
-    })
-    .catch((error) => {
-          console.log("erro ao montar a navbar:", error);
-    })
+    `
+    element.innerHTML = navbar
 }
 
-
+function navBarComponentTemplate() {
+    const element = document.getElementById('navbar-component')
+    fetch("../components/nav-component.html")
+        .then((res) => res.text())
+        .then((nav) => {
+            element.innerHTML = nav
+        })
+        .catch((error) => {
+            console.error("Erro ao montar a navbar: ", error);
+        })
+}
