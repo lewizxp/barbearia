408bbc1c6db1753eab1fd6cc1d617a3667a019bf
lewizxp <luizpedrosantos297@gmail.com>
2024-10-31 17:07:41 -0300

    ffazeer o do celular

--- js/validador.js
+++ js/validador.js
@@ -1,15 +1,35 @@
- function validateName(){
- const element = document.querySelector("#nameInput")
- const message = document .querySelector ("#namehelp")
- element.addEventListener("blur"), (e) => {
-const value = e.target.value
-console.log (value)
-if (value == ""){
-    message.innerHTML = `<span class="text-danger"> valor invalido" </span>`
+validateName()
+validadeRequired ()
 
+function validadeName(){
+    const element = document.querySelector("#nameInput")
+    const message = document.querySelector("#nameHelp")
+    element.addEventListener('blur', (e) =>{
+        const value = e.target.value
+        console.log(value)
+        if(value== ""){
+            message.innerHTML= '<span class= "text-danger"> Valor invalido </span>'
+        }
+        else{
+            message.innerHTML= ''
+        }
+    }
+
+)
 }
-else{
-    message.innerHTML= ""
+
+function validarEmail(){
+    const  element = document.querySelector ("#emailInput")
+    const message = document.querySelector ("#emailHelp")
+    element.addEventListener ('keyup', (e) => {
+        const value = e.target.value
+        console.log (value)
+        if(value == '' ){
+            message.innerHTML = 'Valor invalido' 
+        }
+        else{
+            message.innerHTML='' 
+        }
+    }
+)
 }
- }
-} 
