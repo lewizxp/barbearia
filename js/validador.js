52612850d7d77fef77800c75958ba2623017df89
Programador Full Stack 2024.1 <16133232024.1@senacrjedu.local>
2024-10-31 21:52:48 -0300

    feito

--- js/validador.js
+++ js/validador.js
@@ -1,9 +1,15 @@
-validateName()
-validadeRequired ()
+validadeName()
+validarEmail()
+validartel()
+
 
 function validadeName(){
+    //seleciona o campo que a pessoa vai  digitar com o id de nameInput
     const element = document.querySelector("#nameInput")
+
+    // seleciona o campo onde a mensagem vai aparacer 
     const message = document.querySelector("#nameHelp")
+
     element.addEventListener('blur', (e) =>{
         const value = e.target.value
         console.log(value)
@@ -24,12 +30,28 @@ function validarEmail(){
     element.addEventListener ('keyup', (e) => {
         const value = e.target.value
         console.log (value)
-        if(value == '' ){
-            message.innerHTML = 'Valor invalido' 
+        let error = ''
+        const regexEmail = (/^[0-9] + \@[a-z]+\.([a-z]{3})+$/g)
+        if(!regexEmail.test(value)){
+            error = 'Valor invalido' 
         }
-        else{
-            message.innerHTML='' 
+        
+            message.innerHTML= error
+        })
+}
+
+function validartel(){
+    const element= document.querySelector ("#telInput")
+    const message = document.querySelector ("#telHelp")
+    element.addEventListener('keyup', (e) =>{
+        const value = e.target.value
+        console.log(value)
+        let error = ''
+        const regexTel = (/^[0-9]+$/g)
+        if(!regexTel.test(value)){
+            error = 'valor invalido'
         }
+        message.innerHTML= error
     }
-)
+    )
 }
