3decbfc77bdab60ec980effc6c0a3bbdd89bb462
Programador Full Stack 2024.1 <16133232024.1@senacrjedu.local>
2024-11-05 21:45:32 -0300

    feito

--- js/validador.js
+++ js/validador.js
@@ -1,6 +1,7 @@
 validadeName()
 validarEmail()
 validartel()
+validarpassaword()
 
 
 function validadeName(){
@@ -43,15 +44,31 @@ function validarEmail(){
 function validartel(){
     const element= document.querySelector ("#telInput")
     const message = document.querySelector ("#telHelp")
-    element.addEventListener('keyup', (e) =>{
+    element.setAttribute('maxlength', '15')
+    element.addEventListener('keyup', (e) => {
         const value = e.target.value
-        console.log(value)
-        let error = ''
-        const regexTel = (/^[0-9] {2,3}+$/g)
-        if(!regexTel.test(value)){
-            error = 'valor invalido'
+        const regexTel = /^\([0-9]{2,3}\)\s[0-9]{5}-[0-9]{4}$/g
+        let error = ""
+        if (!regexTel.test(value)) {
+            error = 'Telefone invalido. Formato: "(999) 99999-9999"'
         }
-        message.innerHTML= error
+        message.innerHTML = error
+        //setContentElement("#telHelp", error)
+    })
+}
+function validarpassaword(){
+    const element = document .querySelector("passwordInput")
+    const message = document.querySelector("passwordHelp")
+    element.setAttribute('maxlength', '8')
+    element.addEventListener('keyup', (e) => {
+    const value = e.target.value
+    const regexpassaword = (/^[0-9] + [a-z]$/g)
+    let erro = ""
+    if(!regexpassaword.test(value)){
+        error = 'senha invalida , tamanho maximo de 8 caracteres'
     }
-    )
+    message.innerHTML = error
+
+
+    })
 }
