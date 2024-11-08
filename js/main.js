99f823c273f316dc0cdb53a05f1e07b3bcd63ee7
Programador Full Stack 2024.1 <16133232024.1@senacrjedu.local>
2024-11-07 21:44:45 -0300

    feito

--- js/main.js
+++ js/main.js
@@ -1,52 +1,21 @@
-//navBarComponent()
-navBarComponentTemplate()
+import { validateEmail, validateName, validatePassword, validateTel } from './service/validator.js'
+import { navBarComponentTemplate } from "./../components/nav-component/nav-component.js"
 
-function navBarComponent() {
-    const element = document.getElementById('navbar-component')
-    const navbar = `
-    <nav class="navbar navbar-expand-lg fixed-top">
-        <div class="container">
-            <!-- <a class="navbar-brand" href="#">Navbar</a> -->
-            <img class="navbar-toggler border-0" src="./imagem/barbershop-logo.jpg" height="40" alt="">
-            <button class="navbar-toggler bg-yellow" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
-                aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-            <div class="collapse navbar-collapse" id="navbarNav">
-                <ul class="navbar-nav me-auto">
-                    <li class="nav-item">
-                        <a class="nav-link" href="index.html">Inicio</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="index.html#services">Serviços</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="index.html#about">Sobre Nós</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="index.html#contact">Contato</a>
-                    </li>
-                </ul>
-                <ul class="navbar-nav">
-                    <li class="nav-item">
-                        <a class="nav-link" href="login.html">Entrar</a>
-                    </li>
-                </ul>
-            </div>
-        </div>
-    </nav>
-    `
-    element.innerHTML = navbar
+navBarComponentTemplate()
+validateName()
+validateEmail()
+validateTel()
+validatePassword()
+
+//Função para pegar os elementos
+function getElement(elementSelector) {
+    return document.querySelector(elementSelector)
 }
 
-function navBarComponentTemplate() {
-    const element = document.getElementById('navbar-component')
-    fetch("../components/nav-component.html")
-        .then((res) => res.text())
-        .then((nav) => {
-            element.innerHTML = nav
-        })
-        .catch((error) => {
-            console.error("Erro ao montar a navbar: ", error);
-        })
+//Função para preencher do conteúdo do elemento
+function setContentElement(elementSelector, content) {
+    const element = getElement(elementSelector)
+    if (element) {
+        element.innerHTML = content
+    }
 }
